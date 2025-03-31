9ae4779d16507e0afac73f4eb68044d5cfb2acae
Stepaniuk Dmytro <120324422+KaktusSd06@users.noreply.github.com>
2025-03-31 16:53:22 +0300

    Changed theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'logic/blocs/theme_bloc.dart';
+import 'core/config/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ThemeBloc(),
-      child: BlocBuilder<ThemeBloc, ThemeMode>(
-        builder: (context, themeMode) {
-          return MaterialApp(
-            title: 'Authenticator App',
-            theme: ThemeData(
-              brightness: Brightness.light,
-              primaryColor: Colors.blue,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            ),
-            darkTheme: ThemeData(
-              brightness: Brightness.dark,
-              primaryColor: Colors.blue,
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            ),
-            themeMode: themeMode,
-            home: const AuthenticatorHomePage(),
-          );
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: HomePage(),
     );
   }
 }
 
-class AuthenticatorHomePage extends StatelessWidget {
-  const AuthenticatorHomePage({super.key});
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Authenticator App'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.light_mode),
-            onPressed: () {
-              context.read<ThemeBloc>().add(ThemeEvent.light);
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.dark_mode),
-            onPressed: () {
-              context.read<ThemeBloc>().add(ThemeEvent.dark);
-            },
-          ),
-        ],
+      appBar: AppBar(title: Text("System Theme")),
+      body: Center(
+        child: Text("The theme follows system settings"),
       ),
-      body: const Center(child: Text("Authenticator App")),
     );
   }
 }
